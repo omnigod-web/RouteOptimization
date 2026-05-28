e4364426ba5d03f0d24bb13fb29ce3753ba4d2e2
Nishant <nishant14022004@gmail.com>
2026-05-29 01:45:30 +0530

    debub

--- Backend/src/controllers/routeController.js
+++ Backend/src/controllers/routeController.js
@@ -17,7 +17,7 @@ export const planRoute =async (req ,res)=>{
          res.status(200).json(result)
     } catch (err) {
         res.status(500).json({
-            message:"something went wrong",
+            message:"something went wronggggggggggg",
             error: err.message
         })
     }
